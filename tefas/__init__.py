fc8795f8d55d25a74931c97a0dd24c96bea6aaef
Burak Yilmaz <burakyilmaz321@gmail.com>
2020-11-27 00:18:13 +0300

    Lazy load hack for setup

--- tefas/__init__.py
+++ tefas/__init__.py
@@ -1,4 +1,13 @@
-from .crawler import Crawler
+# pylint: disable=unused-import
+# pylint: disable=import-outside-toplevel
 
 __version__ = "0.2"
 __all__ = ["Crawler", "__version__"]
+
+
+def __getattr__(name):
+    # PEP-562: Lazy loaded attributes on python modules
+    if name == "Crawler":
+        from tefas.crawler import Crawler
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
