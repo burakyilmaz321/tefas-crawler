e5be278e895ed711eab647fd137b8333479dd929
Burak Yilmaz <burakyilmaz321@gmail.com>
2021-07-20 15:30:17 +0300

    fix the bug occurs when response is empty
    Some days, or some funds has no data. In that case, tefas
    returns an empty list. Handle this case by explicitly passing
    the columns argument of pandas.DataFrame.
    
    Add a unit test for this case

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,6 +1,14 @@
+from unittest.mock import MagicMock
+
 from tefas import Crawler
 
 
 def test_crawler():
     crawler = Crawler()
     assert crawler
+
+def test_empty_result():
+    """Test the client when POST to tefas returns empty list"""
+    Crawler._do_post = MagicMock(return_value=[])
+    crawler = Crawler()
+    crawler.fetch(start="2020-11-20")
